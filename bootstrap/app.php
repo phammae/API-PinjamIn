df71190d00c55a9568763c94aecaae63cad87a40
Mas Bima <bimfikhri@gmail.com>
2025-11-28 23:43:25 +0700

    feat: RoleSeeder

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Middlewares\EnableCors;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\Permission\Middleware\RoleMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
+        $middleware->alias([
+            'enable.cors' => EnableCors::class,
+            'role' => RoleMiddleware::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
